d2ea2325f1df245e82326d0579387c75acd68cce
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-06-26 19:44:48 +0200

    lasts steps on thems

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -14,3 +14,33 @@ const getInitialTheme = () => {
     }
     return 'light'
 }
+
+export const ThemeContext = createContext()
+
+export const ThemeProvider = ({initialTheme, children}) => {
+    const [theme, setTheme] = useState(getInitialTheme)
+
+    const rawSetTheme = (theme) => {
+        const root = window.document.documentElement;
+        const isDark = theme === 'dark'
+
+        root.classList.remove(isDark ? 'light' : 'dark')
+        root.classList.add(theme)
+
+        localStorage.setItem('color-theme', theme)
+    }
+
+    if (initialTheme) {
+        rawSetTheme(initialTheme)
+    }
+
+    useEffect(()=> {
+        rawSetTheme(theme)
+    },[theme])
+
+    return (
+        <ThemeContext.Provider value={{theme,setTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
