4fc4e02d54a243c4d52ba1e575b9eae1b922bb3e
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-06-26 19:28:03 +0200

    dark and light modes css and implementing thems

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: 'var(--color-bg-primary)',
+        secondary: 'var(--color-bg-secondary)',
+        button: 'var(--color-bg-button)',
+      },
+      textColor: {
+        accent: 'var(--color-text-accent)',
+        primary: 'var(--color-text-primary)',
+        secondary: 'var(--color-text-secondary)',
+        btnText: 'var(--color-bg-secondary)',
+      },
+      borderColor: {
+        primary: 'var(--color-bg-primary)',
+        secondary: 'var(--color-bg-secondary)',
+        input: 'var(--color-bg-input)',
+        accent: 'var(--color-text-accent)',
+      },
+    },
   },
   plugins: [],
-}
+};
