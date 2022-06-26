3d33cddfe9aa932e25b112432fe8283753dc799c
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-06-26 19:34:07 +0200

    initial theme

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,1 +1,16 @@
 import React, {useState, useEffect, createContext} from "react";
+
+const getInitialTheme = () => {
+    if (typeof window !== 'undefined' && window.localStorage) {
+        const storedPrefs = window.localStorage.getItem('color-theme')
+        if (typeof storedPrefs === 'string') {
+            return storedPrefs
+        }
+
+        const userMedia = window.matchMedia('(prefers-color-scheme: dark)')
+        if (userMedia.matches) {
+            return 'dark'
+        } 
+    }
+    return 'light'
+}
