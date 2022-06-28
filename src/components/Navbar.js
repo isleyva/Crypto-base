10f409c08e4b0f7c4ecee90f217d0f404eca5d41
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-06-28 18:58:49 +0200

    menu icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import ThemeToggle from "./ThemeToggle"
-
+import { AiOutlineMenu } from "react-icons/ai"
 
 
 function Navbar() {
@@ -17,7 +17,11 @@ function Navbar() {
         <Link to="/singin">Sing In</Link>
        <Link to="/singup">Sing Up</Link>
         </div> 
-    
+          {/* Menu icons */}
+          <div>
+            <AiOutlineMenu className="text-primary text-2xl mr-2" />
+           </div>
+           {/* Mobile Menu */}
     </div>
   )
 }
