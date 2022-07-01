b5d81725a636576d7afc843f97978a8d2c447115
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-01 02:44:09 +0200

    inserting api url

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ import axios from "axios";
 import { useState } from "react";
 
 
+
 function App() {
   const [coins, setCoins] = useState([]);
-  
+  const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
   return (
     <ThemeProvider>
       <Navbar />
