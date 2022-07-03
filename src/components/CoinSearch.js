0ff0c1edba540d4e43c2940d282cf874c1a527a8
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-03 05:08:37 +0200

    api data show

--- src/components/CoinSearch.js
+++ src/components/CoinSearch.js
@@ -1,46 +1,71 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
+import {Sparklines, SparklinesLine} from "react-sparklines";
 
-const CoinSearch = ({coins}) => {
+
+const CoinSearch = ({ coins }) => {
   //console.log (coins)
   return (
     <div>
-        <div>
-            <h1>Search Crypto</h1>
-            <form>
-                <input type="text" name="search" placeholder="Search for a coin..." />
-            </form>
-        </div>
-        <table>
-          <tr>
-            <th></th>
-            <th>#</th>
-            <th>Coin</th>
-            <th></th>
-            <th>Price</th>
-            <th>24h</th>
-            <th>24h</th>
-            <th>MKT</th>
-            <th>Last 7 Days</th>
-          </tr>
-          <tbody>
-            {coins.map((coin )=> (
-              <tr>
-                <td><img src={coin.image} alt={coin.name} /></td>
-                <td><img src={coin.image} alt={coin.name} /></td>
-                <td><img src={coin.image} alt={coin.name} /></td>
-                <td><img src={coin.image} alt={coin.name} /></td>
-                <td><img src={coin.image} alt={coin.name} /></td>
-                <td><img src={coin.image} alt={coin.name} /></td>
-                <td><img src={coin.image} alt={coin.name} /></td>
-                <td><img src={coin.image} alt={coin.name} /></td>
-                <td><img src={coin.image} alt={coin.name} /></td>
-                <td><img src={coin.image} alt={coin.name} /></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div>
+        <h1>Search Crypto</h1>
+        <form>
+          <input type="text" name="search" placeholder="Search for a coin..." />
+        </form>
+      </div>
+      <table>
+        <tr>
+          <th></th>
+          <th>#</th>
+          <th>Coin</th>
+          <th></th>
+          <th>Price</th>
+          <th>24h</th>
+          <th>24h</th>
+          <th>MKT</th>
+          <th>Last 7 Days</th>
+        </tr>
+        <tbody>
+          {coins.map((coin) => (
+            <tr>
+              <td>
+                <AiOutlineStar />
+              </td>
+              <td>
+                {coin.market_cap_rank}
+              </td>
+              <td>
+                <div>
+                  <img src={coin.image} alt={coin.name} />
+                  <p>{coin.name}</p>
+                </div>
+              </td>
+              <td>
+                {coin.symbol}
+              </td>
+              <td>
+                {coin.current_price}
+              </td>
+              <td>
+                {coin.price_change_percentaje_24h}
+              </td>
+              <td>
+               {coin.total_volume}
+              </td>
+              <td>
+                {coin.market_cap}
+              </td>
+              <td>
+               <Sparklines  data={coin.sparkline_in_7d.price}>
+                <SparklinesLine color="teal" />
+               </Sparklines>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default CoinSearch
+export default CoinSearch;
