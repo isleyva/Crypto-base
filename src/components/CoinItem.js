c7a57b237d9e605a2ed4af51f4e48582b750dbbc
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-04 03:27:01 +0200

    toUpperCase

--- src/components/CoinItem.js
+++ src/components/CoinItem.js
@@ -1,45 +1,42 @@
-import React from 'react'
+import React from "react";
 import { AiOutlineStar } from "react-icons/ai";
-import {Sparklines, SparklinesLine} from "react-sparklines";
+import { Sparklines, SparklinesLine } from "react-sparklines";
 
-const CoinItem = ({coin}) => {
+const CoinItem = ({ coin }) => {
   return (
-    <tr className='h-[80px] border-b overflow-hidden'>
-              <td>
-                <AiOutlineStar />
-              </td>
-              <td>
-                {coin.market_cap_rank}
-              </td>
-              <td>
-                <div className='flex items-center'>
-                  <img className="w-6 mr-2 rounded-full" src={coin.image} alt={coin.name} />
-                  <p className="hidden sm:table-cell">{coin.name}</p>
-                </div>
-              </td>
-              <td>
-                {coin.symbol}
-              </td>
-              <td>
-                {coin.current_price}
-              </td>
-              <td>
-                {coin.price_change_percentage_24h > 0 ? (<p className='text-green-600'> {coin.price_change_percentage_24h}</p>): (<p className='text-red-600'> {coin.price_change_percentage_24h}</p>)}
-               
-              </td>
-              <td className='w-[180px] hidden md:table-cell'>
-               {coin.total_volume}
-              </td>
-              <td className='w-[180px] hidden sm:table-cell'>
-                {coin.market_cap}
-              </td>
-              <td>
-               <Sparklines  data={coin.sparkline_in_7d.price}>
-                <SparklinesLine color="teal" />
-               </Sparklines>
-              </td>
-            </tr>
-  )
-}
+    <tr className="h-[80px] border-b overflow-hidden">
+      <td>
+        <AiOutlineStar />
+      </td>
+      <td>{coin.market_cap_rank}</td>
+      <td>
+        <div className="flex items-center">
+          <img
+            className="w-6 mr-2 rounded-full"
+            src={coin.image}
+            alt={coin.name}
+          />
+          <p className="hidden sm:table-cell">{coin.name}</p>
+        </div>
+      </td>
+      <td>{coin.symbol.toUpperCase()}</td>
+      <td>{coin.current_price}</td>
+      <td>
+        {coin.price_change_percentage_24h > 0 ? (
+          <p className="text-green-600"> {coin.price_change_percentage_24h}</p>
+        ) : (
+          <p className="text-red-600"> {coin.price_change_percentage_24h}</p>
+        )}
+      </td>
+      <td className="w-[180px] hidden md:table-cell">{coin.total_volume}</td>
+      <td className="w-[180px] hidden sm:table-cell">{coin.market_cap}</td>
+      <td>
+        <Sparklines data={coin.sparkline_in_7d.price}>
+          <SparklinesLine color="teal" />
+        </Sparklines>
+      </td>
+    </tr>
+  );
+};
 
-export default CoinItem
+export default CoinItem;
