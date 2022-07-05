581214d9d9d803ac03be1b1367f9dc7fa33edc27
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-05 03:39:29 +0200

    API call as ready

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,14 +1,18 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Trending = () => {
-    const [trending, setTrending] = useState([])
-    const url = 'https://api.coingecko.com/api/v3/search/trending'
-    
-    return (
-    <div>Trending</div>
-  )
-}
+  const [trending, setTrending] = useState([]);
 
-export default Trending
+  const url = "https://api.coingecko.com/api/v3/search/trending";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setTrending(response.data.coins);
+      console.log(response.data.coins);
+    });
+  }, []);
+  return <div>Trending</div>;
+};
+
+export default Trending;
