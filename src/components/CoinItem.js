31e0cf09a97ba627ad50e1c97e514bbc8f0e9831
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-06 21:53:31 +0200

    fixing more details

--- src/components/CoinItem.js
+++ src/components/CoinItem.js
@@ -23,7 +23,7 @@ const CoinItem = ({ coin }) => {
       <td>${coin.current_price.toLocaleString()}</td>
       <td>
         {coin.price_change_percentage_24h > 0 ? (
-          <p className="text-green-600"> {coin.price_change_percentage_24h.toFixed(2)}%</p>
+          <p className="text-green-600"> +{coin.price_change_percentage_24h.toFixed(2)}%</p>
         ) : (
           <p className="text-red-600"> {coin.price_change_percentage_24h.toFixed(2)}%</p>
         )}
