b0edf57abd0aa1672e3377a8ff359cba2fe70cc1
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-06 22:07:55 +0200

    setting axios and usestate in the function

--- src/routes/CoinPage.js
+++ src/routes/CoinPage.js
@@ -1,6 +1,13 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
 
 const CoinPage = () => {
+  const [coin, setCoin] = useState({})
+   
+  const url = "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true"
+  
+  
   return (
     <div>CoinPage</div>
   )
