f0c70a984c43f09081323ddc7ec14849cf90da97
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-07 02:51:03 +0200

    price

--- src/routes/CoinPage.js
+++ src/routes/CoinPage.js
@@ -1,32 +1,37 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const CoinPage = () => {
-  const [coin, setCoin] = useState({})
-   
-  const url = "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true"
-  
-  useEffect(() => {
-    axios .get(url).then((response) => {
-        setCoin(response.data)
-       // console.log(response.data)
-        }
-    )
-  }, [url])
+  const [coin, setCoin] = useState({});
 
+  const url =
+    "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoin(response.data);
+      // console.log(response.data)
+    });
+  }, [url]);
 
   return (
-    <div className='rounded-div'>
+    <div className="rounded-div">
+      <div>
+        <img src={coin.image?.large} alt="/" />
+      </div>
+      <div>
+        <p>{coin.name} price</p>
+        <p>({coin.symbol.toUpperCase()} / USD)</p>
+      </div>
+      <div>
         <div>
-           <img src={coin.image?.large} alt="/"/> 
+          <div>
+            <p>{coin.market_data.current_price ? (<p>${ coin.market_data.current_price.usd.toLocaleString() }</p>) : null}</p>
+          </div>
         </div>
-        <div>
-            <p>{coin.name} price</p>
-            <p>({coin.symbol.toUpperCase()} / USD)</p>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CoinPage
+export default CoinPage;
