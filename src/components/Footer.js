fa3e68033cc587b430f26cde7443c274be8369c3
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-13 02:11:16 +0200

    imports

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import { FaFacebook, FaGithub, FaTiktok, FaTwitter } from 'react-icons/fa'
+import ThemeToggle from './ThemeToggle'
+
+
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
