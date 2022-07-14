ef494ebfdf35a217c36f02aff9784c8965364ca9
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-14 18:35:29 +0200

    html

--- src/routes/Account.js
+++ src/routes/Account.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import SavedCoin from '../components/SavedCoin'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <div>
+     <div>
+      <div>
+        <h1>Account</h1>
+        <div>
+          <p>Welcome, User</p>
+        </div>
+      </div>
+        <div>
+          <button>Sign Out</button>
+        </div>
+      </div> 
+      <div>
+        <div>
+          
+            <h1>Wacht list</h1>
+            <SavedCoin/>
+          
+      </div>
+    </div>
+    </div>
   )
 }
 
