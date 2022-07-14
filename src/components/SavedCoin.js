11511e205b35445cf19a1bcf6b8f86e9ed4f89db
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-14 18:45:03 +0200

    Link and useStatet

--- src/components/SavedCoin.js
+++ src/components/SavedCoin.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Savedcoin = () => {
-  return (
-    <div>Savedcoin</div>
+        const [coins, setCoins] = useState([]);
+  
+    return (
+    <div>
+        <p>You don't have any coins saved. Please save a coin to add it to your
+          watch list.  <Link to="/" >Click here to search for a coin.</Link></p>
+    </div>
   )
 }
 
