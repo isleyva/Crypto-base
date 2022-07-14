21fba3e9cf31a5c5355d9b3f1bc7539d3465c2e8
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-14 20:00:39 +0200

    styling 2

--- src/routes/Account.js
+++ src/routes/Account.js
@@ -12,12 +12,12 @@ const Account = () => {
           </div>
         </div>
         <div>
-          <button>Sign Out</button>
+          <button className='border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl'>Sign Out</button>
         </div>
       </div>
-      <div>
-        <div>
-          <h1>Wacht list</h1>
+      <div className='flex justfiy-between items-center my-12 py-8 rounded-div'>
+        <div className='w-full min-h-[300px]'>
+          <h1 className='text-2xl font-bold py-4'>Wacht list</h1>
           <SavedCoin />
         </div>
       </div>
