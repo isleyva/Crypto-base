6d327e40ed54ecdae85365552a8c30b8fd490c07
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-14 23:25:34 +0200

    map in coin

--- src/components/SavedCoin.js
+++ src/components/SavedCoin.js
@@ -7,8 +7,23 @@ const Savedcoin = () => {
   
     return (
     <div>
-        <p>You don't have any coins saved. Please save a coin to add it to your
-          watch list.  <Link to="/" >Click here to search for a coin.</Link></p>
+        {coins.length === 0 ? (        <p>You don't have any coins saved. Please save a coin to add it to your
+          watch list.  <Link to="/" >Click here to search for a coin.</Link></p>) : (
+            <table>
+                <thead>
+                  <tr>
+                    <th>Rank #</th>
+                    <th>Coin</th>
+                    <th>Remove</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {coins.map((coin) => (
+                    <tr key={coin.id}></tr>
+                  ))}
+                </tbody>
+            </table>
+          )}
     </div>
   )
 }
