63cd0f52ec24d9ef61cf5da9f9f9896ebc147ab7
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-18 02:50:09 +0200

    setting keys from firebase

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBYTPsNolC-C6M3kMuhxISSBOY8PhxFJhw",
-  authDomain: "cryptobase-1860c.firebaseapp.com",
-  projectId: "cryptobase-1860c",
-  storageBucket: "cryptobase-1860c.appspot.com",
-  messagingSenderId: "1051682645850",
-  appId: "1:1051682645850:web:67e66bc22ce3241dc1b563",
-  measurementId: "G-Q49BEHJLQ9"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  
 };
 
 // Initialize Firebase
