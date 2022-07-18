2d1c5b471c2faf880b72be774b47ca9dcaa014eb
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-18 04:15:42 +0200

    sing in logic

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,25 +1,23 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import { auth, db } from "../firebase";
 import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
 } from "../firebase/auth";
-import {doc, setDoc} from "../firebase/firestore";
+import { doc, setDoc } from "../firebase/firestore";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-        const signUp = (email, password) => {
-             createUserWithEmailAndPassword(auth, email, password);
-             return setDoc (doc(db, 'users', email), {
-                watchList: [],
-             });
-            };
+  const signUp = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password);
+    return setDoc(doc(db, "users", email), {
+      watchList: [],
+    });
+  };
+  const signIn = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password);
+  }
 };
-
-
-
-
-
