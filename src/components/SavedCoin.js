3b384e8c9982a411cd3d5f3f6d909b0fb9471542
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-18 02:02:22 +0200

    last html and some styling

--- src/components/SavedCoin.js
+++ src/components/SavedCoin.js
@@ -1,42 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Savedcoin = () => {
-        const [coins, setCoins] = useState([]);
-  
-    return (
-    <div>
-        {coins.length === 0 ? (        <p>You don't have any coins saved. Please save a coin to add it to your
-          watch list.  <Link to="/" >Click here to search for a coin.</Link></p>) : (
-            <table>
-                <thead>
-                  <tr>
-                    <th>Rank #</th>
-                    <th>Coin</th>
-                    <th>Remove</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {coins.map((coin) => (
-                    <tr key={coin.id}>
-                      <td>{coin?.rank}</td>
-                      <td>
-                        <Link to={`/coin/${coin.id}`}>
-                         <div>
-                           <img src={coin?.image} alt="/" /> 
-                            <p>{coin?.name}</p>
-                            <p>{coin?.symbol.toUpperCase()}</p>
-                          </div> 
-                        </Link>
-                      </td>
-                      </tr>
-                  ))}
-                </tbody>
-            </table>
-          )}
-    </div>
-  )
-}
+  const [coins, setCoins] = useState([]);
 
-export default Savedcoin
+  return (
+    <div>
+      {coins.length === 0 ? (
+        <p>
+          You don't have any coins saved. Please save a coin to add it to your
+          watch list. <Link to="/">Click here to search for a coin.</Link>
+        </p>
+      ) : (
+        <table className="w-full border-collapse text-center">
+          <thead>
+            <tr className="border-b">
+              <th className="px-4">Rank #</th>
+              <th className="text-left">Coin</th>
+              <th className="text-left">Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {coins.map((coin) => (
+              <tr key={coin.id}>
+                <td>{coin?.rank}</td>
+                <td>
+                  <Link to={`/coin/${coin.id}`}>
+                    <div>
+                      <img src={coin?.image} alt="/" />
+                      <div>
+                      <p>{coin?.name}</p>
+                      <p>{coin?.symbol.toUpperCase()}</p>
+                      </div>
+                    </div>
+                  </Link>
+                  <td>
+                    <AiOutlineClose className="cursor-pointer"/>
+                  </td>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default Savedcoin;
