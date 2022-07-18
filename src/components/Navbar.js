bc4f9d4ac8adfe3ff01694a19c47445f5c58444a
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-07-19 00:43:06 +0200

    change for be registred

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,10 +34,21 @@ function Navbar() {
 
       {user?.email ? (
         <div>
-          <Link to="/account">Account</Link>
+          <Link to="/account" className="p-4">Account</Link>
           <button onClick={handleSignOut}>Sign Out</button>
         </div>
-      ) : ()}
+      ) : (  <div className='hidden md:block'>
+      <Link to='/signin' className='p-4 hover:text-accent'>
+        Sign In
+      </Link>
+      <Link
+        to='/signup'
+        className='bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl'
+      >
+        Sign Up
+      </Link>
+    </div>
+  )}
    
       {/* Menu icons */}
       <div onClick={handleNav} className="block md:hidden cursor-pointer z-10">
